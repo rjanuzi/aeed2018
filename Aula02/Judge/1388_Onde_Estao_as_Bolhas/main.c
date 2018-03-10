efdf438a7e57ee7e4ad5b4637ce052cc5acd52be
Rafael B. Januzi <rjanuzi@gmail.com>
2018-03-09 23:49:44 -0300

    Alguns testes funcionando

--- Aula02/Judge/1388_Onde_Estao_as_Bolhas/main.c
+++ Aula02/Judge/1388_Onde_Estao_as_Bolhas/main.c
@@ -1,25 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_LOOPS 100000
+
 int main( void )
 {
-
-	int itensCount, loopCount;
-	int* changesCount;
+	int itensCount, loopCount, totalSwapsExpected = 0, temp;
+	int* swapsDoneCount;
 	int* result;
 
-	itensCount = 3;
-	loopCount = 1;
+	//Le primeira linha
+	itensCount = 6;
+	loopCount = 5;
 	result = malloc(sizeof(int) * itensCount);
-	changesCount = malloc(sizeof(int) * loopCount);
 	
-	changesCount[0] = 1;
+	//Le segunda linha
+	swapsDoneCount = malloc(sizeof(int) * loopCount);
+	swapsDoneCount[0] = 2;
+	swapsDoneCount[1] = 2;
+	swapsDoneCount[2] = 2;
+	swapsDoneCount[3] = 2;
+	swapsDoneCount[4] = 1;
+	
+	//Coloca itens ordenados no array result
+	for(int i = 0; i < itensCount; i++)
+		result[i] = (i+1);
 
+	//Calcula quantos swapsDones devem acontencer
 	for(int loop = 0; loop < loopCount; loop++)
+		totalSwapsExpected += swapsDoneCount[loop];
+	
+	//Do final do array para o inicio, realiza trocas 
+	//quando o elemento anterior for menor
+	for(int swapsDone = 0, loopsMaxCount = 0; swapsDone < totalSwapsExpected && loopsMaxCount < MAX_LOOPS; loopsMaxCount++)
 	{
-		//TODO
-		changesCount[0];
-	} 
+		for(int i = (itensCount-1); i > 0 && swapsDone < totalSwapsExpected; i--)
+		{
+			if( result[i-1] < result[i] )
+			{
+				//swapsDone
+				temp = result[i-1];
+				result[i-1] = result[i];
+				result[i] = temp;
+				
+				swapsDone++;
+			}
+			else
+				continue; //Deixa esse elemento pra la e vai atras do proximo.
+		}
+	}
+	
+	for(int i = 0; i < itensCount; i++)
+		printf("%d ", result[i]);
 
 	return 0;
 }
