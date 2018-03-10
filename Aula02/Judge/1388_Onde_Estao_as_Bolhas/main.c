d8ef18627d1115316582fe94820240e9181c45ce
Rafael B. Januzi <rjanuzi@gmail.com>
2018-03-10 00:10:11 -0300

    Resposta Incorreta

--- Aula02/Judge/1388_Onde_Estao_as_Bolhas/main.c
+++ Aula02/Judge/1388_Onde_Estao_as_Bolhas/main.c
@@ -5,53 +5,56 @@
 
 int main( void )
 {
-	int itensCount, loopCount, totalSwapsExpected = 0, temp;
-	int* swapsDoneCount;
+	int itensCount, loopCount, totalSwapsExpected = 0, temp, i, swapsDone, loopsMaxCount;
 	int* result;
 
-	//Le primeira linha
-	itensCount = 6;
-	loopCount = 5;
-	result = malloc(sizeof(int) * itensCount);
-	
-	//Le segunda linha
-	swapsDoneCount = malloc(sizeof(int) * loopCount);
-	swapsDoneCount[0] = 2;
-	swapsDoneCount[1] = 2;
-	swapsDoneCount[2] = 2;
-	swapsDoneCount[3] = 2;
-	swapsDoneCount[4] = 1;
-	
-	//Coloca itens ordenados no array result
-	for(int i = 0; i < itensCount; i++)
-		result[i] = (i+1);
-
-	//Calcula quantos swapsDones devem acontencer
-	for(int loop = 0; loop < loopCount; loop++)
-		totalSwapsExpected += swapsDoneCount[loop];
-	
-	//Do final do array para o inicio, realiza trocas 
-	//quando o elemento anterior for menor
-	for(int swapsDone = 0, loopsMaxCount = 0; swapsDone < totalSwapsExpected && loopsMaxCount < MAX_LOOPS; loopsMaxCount++)
+	while(1)
 	{
-		for(int i = (itensCount-1); i > 0 && swapsDone < totalSwapsExpected; i--)
+		//Le primeira linha
+		scanf("%d %d", &itensCount, &loopCount);
+		
+		if(itensCount == 0 || loopCount == 0)
+			break;
+
+		result = calloc(sizeof(int) * itensCount, sizeof(int));
+		
+		//Le segunda linha
+		for(i = 0; i < loopCount; i++)
 		{
-			if( result[i-1] < result[i] )
-			{
-				//swapsDone
-				temp = result[i-1];
-				result[i-1] = result[i];
-				result[i] = temp;
-				
-				swapsDone++;
-			}
-			else
-				continue; //Deixa esse elemento pra la e vai atras do proximo.
+			scanf("%d", &temp);
+			totalSwapsExpected += temp;
 		}
+		
+		//Coloca itens ordenados no array result
+		for(i = 0; i < itensCount; i++)
+			result[i] = (i+1);
+		
+		//Do final do array para o inicio, realiza trocas 
+		//quando o elemento anterior for menor
+		for(swapsDone = 0, loopsMaxCount = 0; swapsDone < totalSwapsExpected && loopsMaxCount < MAX_LOOPS; loopsMaxCount++)
+		{
+			for(i = (itensCount-1); i > 0 && swapsDone < totalSwapsExpected; i--)
+			{
+				if( result[i-1] < result[i] )
+				{
+					//swapsDone
+					temp = result[i-1];
+					result[i-1] = result[i];
+					result[i] = temp;
+					
+					swapsDone++;
+				}
+				else
+					continue; //Deixa esse elemento pra la e vai atras do proximo.
+			}
+		}
+		
+		for(i = 0; i < itensCount; i++)
+			printf("%d ", result[i]);
+		printf("\n");
+		
+		free(result);
 	}
-	
-	for(int i = 0; i < itensCount; i++)
-		printf("%d ", result[i]);
 
 	return 0;
 }
