31879a7b18dcc5dfae280854a64bd730b42780f5
Rafael B. Januzi <rjanuzi@gmail.com>
2018-05-17 17:39:55 -0300

    Testes com filtragem de chaves com poucos dados

--- Projeto/preprocess.py
+++ Projeto/preprocess.py
@@ -10,6 +10,8 @@ SALES_COL = 6
 inputFilePath = "C:\\Users\\Rafael\\Desktop\\aeed2018\\Projeto\\Dataset\\Ztco0018 2010_2015.xlsx"
 outputFilePath = "C:\\Users\\Rafael\\Desktop\\aeed2018\\Projeto\\treated_input.xlsx"
 
+ENTRIES_MIN_COUNT = 150
+
 def calcPeriod(day, month):
         "This function calculate the period of year"
         month_part = calcMonthPart(day)
@@ -43,36 +45,75 @@ print("Loading input\n")
 
 # Load Workbook. data_only = True force the formulas calculations
 wb = load_workbook(filename = inputFilePath, data_only = True) # Full input
-#wb = load_workbook(filename = "input_min.xlsx", data_only = True) # Reduced input
 
 # Get the desired Sheet
 ws = wb['Plan1']
 
 # Create new dictionary
 sales_map = dict()
+clients = []
+families = []
 
 print("Processing input\n")
 
 # Start at row 2 to skip the table title
 for row in ws.iter_rows(min_row = 2, max_col = 13):
+    tempClientName = row[CLIENT_COL].value.encode("utf-8") # Encode to solve a dataset problem
+    tempFamilyName = row[FAMILY_COL].value.encode('utf-8') # Encode to solve a dataset problem
+    try:
+        # Generate the map key
         temp_key = str(row[DATE_COL].value.year) + "," + \
                 str( calcPeriod(row[DATE_COL].value.day, row[DATE_COL].value.month) )  + "," + \
-                str(row[CLIENT_COL].value) + "," + \
-                str(row[FAMILY_COL].value)
+                str(tempClientName) + "," + \
+                str(tempFamilyName)
+
+        # Adjust quantity to the map
         if temp_key in sales_map:
                 sales_map[temp_key] = sales_map[temp_key] + row[SALES_COL].value
         else:
                 sales_map[temp_key] = row[SALES_COL].value
 
-print("Saving input\n")
+        # Fill the clients and families list for future use
+        if tempClientName not in clients:
+            clients.append(tempClientName)
+        if tempFamilyName not in families:
+            families.append(tempFamilyName)
+
+    except ValueError:
+        print "Oops! Error interpreting the file"
+
+wb.close()
+
+# Start filter clients and families with low data
+print("Filtering treted data\n")
+
+print("Removing clients and families with low data")
+
+keysList = list(sales_map.keys())
+for client in clients:
+    keysWithClient = filter(lambda k: client in k, keysList)
+    if(len(keysWithClient) < ENTRIES_MIN_COUNT):
+        for keyToRemove in keysWithClient:
+            if keyToRemove in sales_map:
+                del sales_map[keyToRemove]
+
+keysList = list(sales_map.keys())
+for family in families:
+    keysWithFamily = filter(lambda k: family in k, keysList)
+    if(len(keysWithFamily) < ENTRIES_MIN_COUNT):
+        for keyToRemove in keysWithFamily:
+            if keyToRemove in sales_map:
+                del sales_map[keyToRemove]
+
+print("Saving treated_input\n")
 
 res = Workbook()
 res_sheet = res.active
 
 for key, value in sales_map.items():
-        splited_key = key.split(",")
-        splited_key.append(value)
-        res_sheet.append(splited_key)
+            splited_key = key.split(",")
+            splited_key.append(value)
+            res_sheet.append(splited_key)
 
 res.save(outputFilePath)
 
---
+++ Projeto/teste.py
@@ -0,0 +1,2 @@
+lst = ['a', 'ab', 'abc', 'bac']
+print(filter(lambda k: 'ab' in k, lst))
