d5e7e1ec3965e820153738711f44544eed3a33e1
Rafael B. Januzi <rjanuzi@gmail.com>
2018-05-23 00:04:11 -0300

    Primeira implementacao de previsoes com o knn

--- Projeto/knn.py
+++ Projeto/knn.py
@@ -1,6 +1,9 @@
 import math
 import random
+from operator import itemgetter
 import input_loader
+
+DEFAULT_ACCEPTED_ERROR = 1000
 
 def euclideanDist(A, B):
     sum = 0.0
@@ -12,28 +15,47 @@ def euclideanDist(A, B):
 # Calc dist between two itens "'year,period,family'"
 def calcDist(x1, x2):
     x1Splited = x1.split(",")
-    x1Vals = [int(x1Splited[0]), int(x1Splited[1]), x1Splited[2]]
     x2Splited = x2.split(",")
-    x2Vals = [int(x2Splited[0]), int(x2Splited[1]), x2Splited[2]]
+    x1Vals = [int(x1Splited[0]), int(x1Splited[1])]
+    x2Vals = [int(x2Splited[0]), int(x2Splited[1])]
 
-    if x1Vals[2] != x2Vals[2]:
-        return float("inf")
-    else:
-        return euclideanDist( \
-            [int(x1Vals[0]),int(x1Vals[1])], \
-            [int(x2Vals[0]),int(x2Vals[1])] )
+    return euclideanDist([x1Vals[0],x1Vals[1]], [x2Vals[0],x2Vals[1]])
 
-def knn(k, dataset, new, error):
-    datasetKeys = dataset.keys()
+def knn(k, dataset, new):
+    # Filter the keys in dataset with family of interest
+    familyOfInterest = new.split(",")[2]
+    datasetKeysWithFamily = filter(lambda f: familyOfInterest in f, dataset.keys())
+
+    # Each element in distances list is a list like (key, distToReference)
     distances = []
-
-    for key in dataset:
+    for key in datasetKeysWithFamily:
         distances.append((key, calcDist(key, new)))
 
-    print(distances)
+    distances = sorted(distances, key=itemgetter(1))
 
+    if k < len(distances):
+        kElements = distances[0:k]
+    else:
+        kElements = distances
+
+    print("K-Distances: " + str(kElements))
+
+    sum = .0
+    for element in kElements:
+        sum += dataset[element[0]]
+
+    print("Sum: " + str(sum))
+    print("Average: " + str(sum/float(len(kElements))))
+    return sum/float(len(kElements))
+
+# Load inputs
 inputs = input_loader.getInput()
-testingElementKey = random.choice(inputs.keys())
-testingVal = inputs.pop(testingElementKey)
 
-knn(10, inputs, testingElementKey, 0.5)
+for count in range(5):
+    # Pick (remove) a random value from the dataset
+    testingElementKey = random.choice(inputs.keys())
+    testingVal = inputs.pop(testingElementKey)
+    print("Picked: " + str(testingElementKey))
+    print("Val: " + str(testingVal))
+    print("Predicted: " + str(knn(5, inputs, testingElementKey)))
+    print("\n")
--- Projeto/teste.py
+++ Projeto/teste.py
@@ -1,5 +1,3 @@
-a = {"a":1, "b":2, "c":3}
-print(a)
-
-for x in a:
-    print(x)
+x = range(10)
+print(x)
+print(x[0:5])
